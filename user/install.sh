a24144a38e348510ad0786759377b7fc6f9d4405
Sam Kim <samkim@gmail.com>
2018-09-19 10:08:42 -0700

    Add customizations for nvm, serverless, iterm2

--- user/install.sh
+++ user/install.sh
@@ -4,3 +4,7 @@ brew bundle
 cp .gitconfig.user ~
 cp .vimrc.after ~
 cp .vundles.local ~/.vim/
+mkdir -p ~/.zsh.after/
+cp zsh_startup.in ~/.zsh.after/000_iterm2.zsh
+cp zsh_nvm.in ~/.zsh.after/001_nvm.zsh
+cp it2setkeylabel ~/.yadr/bin/
