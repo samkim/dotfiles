7861864a5e8d1d1c31bc04b168d0fa1cb2d4f847
Sam Kim <samkim@gmail.com>
2021-01-21 11:14:48 -0800

    Delta, bat, new config

---
+++ user/.zsh.after/aliases.zsh
@@ -0,0 +1,1 @@
+alias ctags_gen='ripper-tags -R --extra=q --exclude=.git --exclude=log .'
---
+++ user/.zsh.after/fzf.zsh
@@ -0,0 +1,2 @@
+[ -f /usr/local/opt/fzf/shell/completion.zsh ] && source /usr/local/opt/fzf/shell/completion.zsh
+[ -f /usr/local/opt/fzf/shell/key-bindings.zsh ] && source /usr/local/opt/fzf/shell/key-bindings.zsh
