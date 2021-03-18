1a788ef1ab5bd09eb2d3ddab545512b40ae3dfe3
Sam Kim <samkim@gmail.com>
2021-03-18 12:00:48 -0700

    Remove unused and refresh dependencies

--- user/install.sh
+++ user/install.sh
@@ -2,9 +2,8 @@
 
 brew bundle
 cp .gitconfig.user ~
+cp .vimrc.before ~
 cp .vimrc.after ~
 cp .vundles.local ~/.vim/
-mkdir -p ~/.zsh.after/
-cp zsh_startup.in ~/.zsh.after/000_iterm2.zsh
-cp zsh_nvm.in ~/.zsh.after/001_nvm.zsh
-cp it2setkeylabel ~/.yadr/bin/
+cp -R .config ~
+cp -R .zsh.after ~
